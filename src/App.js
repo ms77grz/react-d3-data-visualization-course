bbe40dd2c5df6e9f72744655387fef3c12c4e153
ms77grz <ms77grz@gmail.com>
2021-07-30 12:15:14 +0300

    interaction with react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useCallback } from 'react';
+
+const width = 960;
+const height = 500;
+const circleRadius = 30;
+const initialMousePosition = { x: width / 2, y: height / 2 };
 
 function App() {
+  const [mousePosition, setMousePosition] = useState(initialMousePosition);
+
+  const handleMouseMove = useCallback(
+    event => {
+      const { clientX, clientY } = event;
+      setMousePosition({ x: clientX, y: clientY });
+      console.log(clientX, clientY);
+    },
+    [setMousePosition]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <svg width={width} height={height} onMouseMove={handleMouseMove}>
+      <circle
+        cx={mousePosition.x}
+        cy={mousePosition.y}
+        r={circleRadius}
+      ></circle>
+    </svg>
   );
 }
 
