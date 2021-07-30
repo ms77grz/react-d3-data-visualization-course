d8d9778499c0ea0f1c8d43a371a844b34e971f71
ms77grz <ms77grz@gmail.com>
2021-07-30 09:55:02 +0300

    make a face react d3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { arc } from 'd3';
+
+const width = 960;
+const height = 500;
+
+const centerX = width / 2;
+const centerY = height / 2;
+const strokeWidth = 20;
+
+const eyeOffsetX = 90;
+const eyeOffsetY = 100;
+const eyeRadius = 40;
+
+const mouthWidth = 20;
+const mouthRadius = 140;
+
+const mouthArc = arc()
+  .innerRadius(mouthRadius)
+  .outerRadius(mouthRadius + mouthWidth)
+  .startAngle(Math.PI / 2)
+  .endAngle((Math.PI * 3) / 2);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <svg width={width} height={height}>
+      <g transform={`translate(${centerX}, ${centerY})`}>
+        <circle
+          r={centerY - strokeWidth / 2}
+          fill='yellow'
+          stroke='black'
+          stroke-width={strokeWidth}
+        />
+        <circle cx={-eyeOffsetX} cy={-eyeOffsetY} r={eyeRadius} />
+        <circle cx={eyeOffsetX} cy={-eyeOffsetY} r={eyeRadius} />
+        <path d={mouthArc()} />
+      </g>
+    </svg>
   );
 }
 
