cf88916fa14be6b900697c606c24b7211b5ebccf
Magomed Shamaev <ms77grz@gmail.com>
2021-07-30 23:13:49 +0300

    stylized bar chart - finished

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import { Marks } from './components/Marks';
 
 const width = 960;
 const height = 500;
-const margin = { top: 20, right: 20, bottom: 20, left: 200 };
+const margin = { top: 20, right: 30, bottom: 65, left: 220 };
+const xAxisLabelOffset = 50;
 
 function App() {
   const data = useData();
@@ -20,10 +21,14 @@ function App() {
   const yValue = d => d.Country;
   const xValue = d => d.Population;
 
+  const siFormat = d3.format('.2s');
+  const xAxisTickFormat = tickValue => siFormat(tickValue).replace('G', 'B');
+
   const yScale = d3
     .scaleBand()
     .domain(data.map(yValue))
-    .range([0, innerHeight]);
+    .range([0, innerHeight])
+    .paddingInner(0.15);
 
   const xScale = d3
     .scaleLinear()
@@ -33,14 +38,27 @@ function App() {
   return (
     <svg width={width} height={height}>
       <g transform={`translate(${margin.left},${margin.top})`}>
-        <AxisBottom xScale={xScale} innerHeight={innerHeight} />
+        <AxisBottom
+          xScale={xScale}
+          innerHeight={innerHeight}
+          tickFormat={xAxisTickFormat}
+        />
         <AxisLeft yScale={yScale} />
+        <text
+          className='axis-label'
+          x={innerWidth / 2}
+          y={innerHeight + xAxisLabelOffset}
+          textAnchor='middle'
+        >
+          Poppulation
+        </text>
         <Marks
           data={data}
           xScale={xScale}
           yScale={yScale}
           xValue={xValue}
           yValue={yValue}
+          tooltipFormat={xAxisTickFormat}
         />
       </g>
     </svg>
--- src/components/AxisBottom.js
+++ src/components/AxisBottom.js
@@ -1,9 +1,13 @@
-export const AxisBottom = ({ xScale, innerHeight }) =>
+export const AxisBottom = ({ xScale, innerHeight, tickFormat }) =>
   xScale.ticks().map(tickValue => (
-    <g key={tickValue} transform={`translate(${xScale(tickValue)},0)`}>
+    <g
+      className='tick'
+      key={tickValue}
+      transform={`translate(${xScale(tickValue)},0)`}
+    >
       <line y2={innerHeight} stroke='black' />
       <text style={{ textAnchor: 'middle' }} dy='.71em' y={innerHeight + 3}>
-        {tickValue}
+        {tickFormat(tickValue)}
       </text>
     </g>
   ));
