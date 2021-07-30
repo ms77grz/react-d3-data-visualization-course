c06a2c21af1615121b7b50d34fc4380d3b0e10fb
ms77grz <ms77grz@gmail.com>
2021-07-30 12:43:04 +0300

    loading data into a react program

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import * as d3 from 'd3';
+import { message } from './utils/message';
+
+const csvUrl =
+  'https://gist.githubusercontent.com/ms77grz/913352d4b8209e98a7d894bfcc90a4bf/raw/cssNamedColors.csv';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    d3.csv(csvUrl).then(setData);
+  }, []);
+
+  return <pre>{data ? message(data) : 'Loading...'}</pre>;
 }
 
 export default App;
---
+++ src/utils/message.js
@@ -0,0 +1,9 @@
+import * as d3 from 'd3';
+
+export const message = data => {
+  let message = '';
+  message += Math.round(d3.csvFormat(data).length / 1024) + ' kB\n';
+  message += data.length + ' rows\n';
+  message += data.columns.length + ' columns\n';
+  return message;
+};
