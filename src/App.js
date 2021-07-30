e0fc777f765c466c4509dc52c10772960d9ace50
ms77grz <ms77grz@gmail.com>
2021-07-30 09:36:12 +0300

    make a face with html css svg - finished

--- src/App.js
+++ src/App.js
@@ -3,22 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <svg width='960' height='500'>
+      <circle
+        cx='480'
+        cy='250'
+        r='245'
+        fill='yellow'
+        stroke='black'
+        stroke-width='10'
+      ></circle>
+      <circle cx='350' cy='180' r='50'></circle>
+      <circle cx='600' cy='180' r='50'></circle>
+    </svg>
   );
 }
 
